2be5c49690a80a16196086c6d9e59c10cb2c3301
Andrew Alsing <andrew32alsing@gmail.com>
2023-10-02 10:42:20 -0700

    completed challenge 4 and merged outputs

--- dist/index.js
+++ dist/index.js
@@ -11,9 +11,6 @@ data_json_1.default.forEach((person) => {
     person.first_name = (0, string_lib_1.capitalizeWords)(person.first_name);
     person.last_name = (0, string_lib_1.capitalizeWords)(person.last_name);
 });
-data_json_1.default.forEach((person) => {
-    console.log(person.first_name, person.last_name);
-});
 // Challenge 2
 const options = {
     year: "numeric",
@@ -23,15 +20,29 @@ const options = {
 function formatDate(date) {
     return new Date(date).toLocaleDateString(undefined, options);
 }
-data_json_1.default.forEach((person) => {
-    console.log("Purchased:", formatDate(person.purchased));
-});
 // Challenge 3
 function when(date1, date2) {
     const months = (date2.getFullYear() - date1.getFullYear()) * 12;
     return months - date1.getMonth() + date2.getMonth();
 }
+// Challenge 4
+function formatPhone(phone) {
+    const areaCode = phone.slice(0, 3);
+    const firstThree = phone.slice(3, 6);
+    const lastFour = phone.slice(6, 10);
+    return `(${areaCode}) ${firstThree}-${lastFour}`;
+}
+// Console logs
 data_json_1.default.forEach((person) => {
+    console.log("--------------------");
+    // Challenge 1
+    console.log(person.first_name, person.last_name);
+    // Challenge 2
+    console.log("Purchased:", formatDate(person.purchased));
+    // Challenge 3
     const monthsDiff = when(new Date(person.lastpayment), new Date());
     console.log("Last Payment:", `${monthsDiff} months ago`);
+    // Challenge 4
+    console.log("Phone:", formatPhone(person.phone));
+    console.log("--------------------");
 });
--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,6 @@ data.forEach((person) => {
   person.last_name = capitalizeWords(person.last_name);
 });
 
-data.forEach((person) => {
-  console.log(person.first_name, person.last_name);
-});
-
 // Challenge 2
 const options: Intl.DateTimeFormatOptions = {
   year: "numeric",
@@ -23,17 +19,35 @@ function formatDate(date: string): string {
   return new Date(date).toLocaleDateString(undefined, options);
 }
 
-data.forEach((person) => {
-  console.log("Purchased:", formatDate(person.purchased));
-});
-
 // Challenge 3
 function when(date1: Date, date2: Date): number {
   const months = (date2.getFullYear() - date1.getFullYear()) * 12;
   return months - date1.getMonth() + date2.getMonth();
 }
 
+// Challenge 4
+function formatPhone(phone: string): string {
+  const areaCode = phone.slice(0, 3);
+  const firstThree = phone.slice(3, 6);
+  const lastFour = phone.slice(6, 10);
+  return `(${areaCode}) ${firstThree}-${lastFour}`;
+}
+
+// Console logs
 data.forEach((person) => {
+  console.log("--------------------");
+  // Challenge 1
+  console.log(person.first_name, person.last_name);
+
+  // Challenge 2
+  console.log("Purchased:", formatDate(person.purchased));
+
+  // Challenge 3
   const monthsDiff = when(new Date(person.lastpayment), new Date());
   console.log("Last Payment:", `${monthsDiff} months ago`);
+
+  // Challenge 4
+  console.log("Phone:", formatPhone(person.phone));
+
+  console.log("--------------------");
 });
